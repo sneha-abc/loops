5478f75764340a6e8d8e89200f5f18401d08df9a
sneha-abc <sneha.manjunath0125@gmail.com>
2024-12-27 10:53:57 +0530

    add Loop

---
+++ Loop.java
@@ -0,0 +1,22 @@
+class Loop{
+public static void main(String[] args){
+
+System.out.println("increament");
+
+for(int i=1;i<=10;i++){
+	System.out.println(i);
+}
+
+
+
+
+System.out.println("decrement");
+
+ for(int i=10;i>=0;i--){
+
+ System.out.println(i);}
+ 
+ 
+ 
+}
+}
