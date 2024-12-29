069694f9edc33879ab5e746d02670239226cd440
sneha-abc <sneha.manjunath0125@gmail.com>
2024-12-29 14:38:39 +0530

    move

---
+++ OneNumber.java
@@ -0,0 +1,32 @@
+class OneNumber{
+	public static void main(String[] args){
+		
+		for(int i=1;i<11;i++)
+		{
+			if(i==5)
+				System.out.println(i);
+		}
+		
+		
+	
+        int i = 1;  
+
+        while (i < 11) {  
+            if (i == 8) {  
+                System.out.println(i);  
+            }
+            i++;  
+        }
+		
+		
+		 int j = 1;  
+
+        do {
+            if (j == 2) { 
+                System.out.println(j);  
+            }
+            j++;  
+        } while (j < 11); 
+		
+    }
+}
